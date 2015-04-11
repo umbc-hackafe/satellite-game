d18e5696a9fd2076e848078d3817871ff6e79ace
Dylan Whichard <dylan@whichard.com>
2015-04-11 00:53:01 -0400

    Questionably working client/server API

--- python/api.py
+++ python/api.py
@@ -1,3 +1,4 @@
+import websockets
 import itertools
 import asyncio
 import json
@@ -32,6 +33,27 @@ class API:
 
         return item.id
 
+    def function_call(self, data):
+        """Accepts a client function call request and performs the function as requested.
+
+        Expects `data` to be a dictionary with some of the following keys:
+        id: The id of the object on which to call the function. (required)
+        name: The name of the function to call. (required)
+        args: A list of positional arguments.
+        kwargs: A dictionary of key:value arguments.
+        """
+
+        try:
+            target = self.items_by_id[data["id"]]
+            result = getattr(target, data["name"])(*(data.get("args", [])), **(data.get("kwargs", {})))
+            return {"success": True, "result": result}
+        except KeyError as e:
+            print("Error, object with ID '{}' was not found.".format(data.get("id", None)))
+            return {"success": False, "error": str(e)}
+        except Exception as e:
+            print("Error occurred while calling {}.{}".format(data.get("id", None), data.get("name", None)))
+            raise e
+
     def send_updates(self):
         """Sends all updates for the server"""
 
@@ -43,3 +65,57 @@ class API:
                     out = dict(item)
 
                 client.send(json.dumps(out).encode('UTF-8'))
+
+class Client:
+    def __init__(self, api, socket):
+        self.api = api
+        self.socket = socket
+        self.msg_queue = asyncio.Queue()
+
+    @asyncio.coroutine
+    def read(self):
+        while True:
+            data = yield from self.socket.recv()
+
+            if data is None:
+                print("Closing websocket")
+                break
+
+            data = json.loads(data)
+            try:
+                self.api.function_call(data)
+            except Exception as e:
+                print("Unable to call function from websocket data: ", e)
+
+    @asyncio.coroutine
+    def write(self):
+        while True:
+            msg = yield from self.msg_queue.get()
+            yield from self.websocket.send(msg)
+
+    def send(self, msg):
+        self.msg_queue.put(msg)
+
+class Server:
+    def __init__(self, api, listen, port):
+        self.listen = listen
+        self.port = port
+        self.api = api
+
+    def start(self):
+        """Begin serving websockets to clients, forever."""
+
+        @asyncio.coroutine
+        def websocket_server(websocket, path):
+            client = Client(self.api, websocket)
+            yield from asyncio.wait([client.read(), client.write()])
+
+        start_ws_server = websockets.serve(websocket_server, self.listen, self.port)
+
+        # Maybe don't do this? FIXME/TODO
+        asyncio.get_event_loop().run_until_complete(start_ws_server)
+        asyncio.get_event_loop().run_forever()
+
+    def stop(self):
+        """Close all websockets and stop listening."""
+        pass
